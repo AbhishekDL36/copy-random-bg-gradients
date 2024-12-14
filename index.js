a196a9327e5c3475b81dc7844ea134bb95f03327
Abhishek <abhishekofficials12@gmail.com>
2024-12-14 12:38:38 +0530

    structure ready and random bg color

---
+++ index.js
@@ -0,0 +1,14 @@
+const div = document.querySelector('div')
+
+const btn= document.querySelector('button')
+const input= document.querySelector('input')
+btn.addEventListener("click",()=>{
+    let clr="#"
+    let color = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
+
+    for(let i=0; i<6;i++){
+        clr += color[Math.floor(Math.random()*16)]
+    }
+
+    div.style.background= clr
+})
